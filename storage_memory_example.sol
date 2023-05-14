27dff5034712c397cbeca631e0f4102f441a005d
Ayub Khan <45249173+aykha18@users.noreply.github.com>
2023-05-14 21:56:18 +0530

    Update and rename require_example.sol to storage_memory_example.sol

--- storage_memory_example.sol
+++ storage_memory_example.sol
@@ -3,10 +3,10 @@
 pragma solidity ^0.8.13;
 
 contract VendingMachine {
-    mapping(address=> uint) public balances;
-
+    mapping(address=> uint) public balances;// storage variable 
+    
     function buy(address _address, uint _amount) public {
-        uint eth = 789 / 2;
+        // function arguments are example of memory variable, which persist during function execution.
         require(_amount > eth ether,"Insufficient Eth Balance");
         balances[_address] -=_amount; 
     }
